85d2d2265a361fe66a1152615e145daff87453fd
Fernando Fritzen <fernandofritzen9@gmail.com>
2021-06-07 21:19:59 -0300

    Adicionado despesas e categorias

---
+++ src/main/java/com/example/projetospring/model/Despesa.java
@@ -0,0 +1,96 @@
+package com.example.projetospring.model;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Despesa implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant data;
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Despesa() {
+
+    }
+
+    public Despesa(Long id, String nome, Double preco, Instant data, String descricao, Categoria categoria) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.data = data;
+        this.descricao = descricao;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Instant getData() {
+        return data;
+    }
+
+    public void setData(Instant data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
---
+++ src/main/java/com/example/projetospring/service/DespesaService.java
@@ -0,0 +1,48 @@
+package com.example.projetospring.service;
+
+import com.example.projetospring.model.Despesa;
+import com.example.projetospring.repository.DespesaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DespesaService {
+
+    @Autowired
+    private DespesaRepository repository;
+
+    public List<Despesa> findAll() {
+        return repository.findAll();
+    }
+
+    public Despesa findById(Long id) {
+        Optional<Despesa> obj = repository.findById(id);
+        return obj.get();
+    }
+
+    public Despesa insert(Despesa despesa) {
+        return repository.save(despesa);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Despesa update(Long id, Despesa obj) {
+        Despesa entity = repository.getOne(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(Despesa entity, Despesa obj) {
+        entity.setNome(obj.getNome());
+        entity.setPreco(obj.getPreco());
+        entity.setData(obj.getData());
+        entity.setDescricao(obj.getDescricao());
+        entity.setCategoria(obj.getCategoria());
+    }
+
+}
