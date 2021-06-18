f51194312d26c2eb2633b0d65d1fa4ae609cef7a
SundaeRaul <raulrios0907@outlook.com>
2021-06-17 21:32:21 -0300

    requisições da classe usuario arrumadas

--- src/main/java/com/example/projetospring/controller/UsuarioController.java
+++ src/main/java/com/example/projetospring/controller/UsuarioController.java
@@ -6,6 +6,9 @@ import com.example.projetospring.repository.UsuarioRepository;
 import com.example.projetospring.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +19,7 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService uServ;
-    
+
     @GetMapping
     public List<Usuario> listaUsuarios (){
         return uServ.findUsuarios();
@@ -29,12 +32,21 @@ public class UsuarioController {
     }
 
     @GetMapping(value = "/{Id}")
-    public ResponseEntity<Usuario> findUsuarioById (@PathVariable Long id, @RequestBody Usuario usuarios){
-        usuarios = uServ.findUsuarioID(id, usuarios);
+    public ResponseEntity<Usuario> findUsuarioById (@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails){
+        System.out.println(userDetails);
+        Usuario obj = uServ.findUsuarioID(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Usuario> alterarUsuario (@RequestBody Usuario usuarios, @PathVariable Long id){
+        usuarios = uServ.alterarUsuario(id, usuarios);
         return ResponseEntity.ok().body(usuarios);
     }
 
+
     @DeleteMapping(value = "/{Id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteUsaurio(@PathVariable Long id){
         uServ.deletarUsuario(id);
         return ResponseEntity.noContent().build();
