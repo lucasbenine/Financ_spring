42956b5ffc4bd9b3e240d42af93ea21bf9eb5539
Lucas Benine <lucasbenine41@gmail.com>
2021-09-08 20:14:48 -0300

    testes das classes contasapagarrepository e contasareceberrepository

--- Back/src/main/java/com/example/projetospring/ProjetospringApplication.java
+++ Back/src/main/java/com/example/projetospring/ProjetospringApplication.java
@@ -1,11 +1,5 @@
 package com.example.projetospring;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import java.util.Date;
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,32 +14,4 @@ public class ProjetospringApplication {
     }
     
 }
-    
-    	/*
-        SimpleDateFormat date1 = new SimpleDateFormat("dd/MM/yyyy");
-        
-        System.out.println("Cadastre sua conta abaixo");
-        System.out.println("-------------------------");
-        
-        Scanner sc1 = new Scanner(System.in);
-        
-        Date data = date1.parse("oi");
-        System.out.println(date1.format(data));
-
-        String nome;
-        nome = sc1.next();
-        String categoria;
-        categoria = sc1.next();
-        double valor;
-        valor = sc1.nextDouble();
-        
-        System.out.println("Conta:" + nome);
-        System.out.println("Categoria: " + categoria);
-        System.out.println("Valor: " + valor);
-           
-        sc1.close();
- 
-    
-    
-    */
 
