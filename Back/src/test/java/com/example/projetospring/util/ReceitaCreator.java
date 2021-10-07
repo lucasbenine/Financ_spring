780d55c64ac7b9f8ad2fd51823c473bb461e5051
SundaeRaul <raulrios0907@outlook.com>
2021-10-07 15:38:05 -0300

    push

--- Back/src/test/java/com/example/projetospring/util/ReceitaCreator.java
+++ Back/src/test/java/com/example/projetospring/util/ReceitaCreator.java
@@ -5,18 +5,18 @@ import com.example.projetospring.model.Receitas;
 public class ReceitaCreator {
 
     public static Receitas createReceitaToBeSaved() {
-        Receitas receita = new Receitas(null, "Salário", 2400.00, "salário do meês", UsuarioCreator.createValidUsuario());
-        return receita;
+        Receitas receita1 = new Receitas(null, "Salário", 2400.00, "salário do meês", UsuarioCreator.createValidUsuario());
+        return receita1;
     }
 
     public static Receitas createValidReceita() {
-        Receitas receita = new Receitas(15L, "Freelance", 250.00, "Freela no wordpress", UsuarioCreator.createValidUsuario());
-        return receita;
+        Receitas receita2 = new Receitas(15L, "Freelance", 250.00, "Freela no wordpress", UsuarioCreator.createValidUsuario());
+        return receita2;
     }
 
     public static Receitas createValidUpdatedReceita() {
-        Receitas receita = new Receitas(1L, "Salário2", 2500.00, "salário do mês", UsuarioCreator.createValidUsuario());
-        return receita;
+        Receitas receita3 = new Receitas(1L, "Salário2", 2500.00, "salário do mês", UsuarioCreator.createValidUsuario());
+        return receita3;
     }
 
 }
